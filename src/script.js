8d01f3e15bb621d3b33908c602291f16025ae098
Yavorskiy Maksim <maxfatal@mail.ru>
2023-06-22 22:50:35 +0300

    fix: fix lintr

--- src/script.js
+++
@@ -1,8 +0,0 @@
-let data = new Date(2012, 3, 2);
-let day = data.getFullYear
-function parseDataFromRfc2822(value) {
-    // throw new Error('Not implemented');
-    return new Date(value);
-  }
-
-console.log(day);
